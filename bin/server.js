5527d34409498878ff930abc7902af9256cf964e
root <root@Tofik_Hidayat.localdomain>
2019-04-19 09:10:28 +0700

    curator test server alpha

---
+++ bin/server.js
@@ -0,0 +1,23 @@
+'use rstrict'
+const express = require('express');  
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const app  = express();
+const port = 8080;
+const date  = new Date();
+
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+console.log("curator is live ");
+module.exports = app;
+module.exports.handler = serverless(app);
