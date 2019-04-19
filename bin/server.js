3c577d3ed64ce652e56bbc8e4790467f76b026d5
root <root@Tofik_Hidayat.localdomain>
2019-04-19 08:19:14 +0700

    curator tester

--- bin/server.js
+++ bin/server.js
@@ -1,1 +1,23 @@
-const http  =  require("http");
+const express = require('express');  
+const serverless = require('serverless-http');
+const app  = express();
+const port = 8080;
+
+app.get("/",(req,res)=>{
+	res.send("Hello Curator :)");
+})
+
+/*
+app.listen(port,()=>{
+	console.log(`Curator Listen On port ${port}`)
+})
+*/
+const handler = serverless(app);
+
+module.exports.handler = async (event, context) => {
+  // you can do other things here
+  const result = await handler(event, context);
+  // and here
+  return result;
+};
+
