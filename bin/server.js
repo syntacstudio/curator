4b41d9f6675e4a31e0e804ab83a284947f982f16
root <root@Tofik_Hidayat.localdomain>
2019-04-19 08:53:47 +0700

    curator test server alpha

--- bin/server.js
+++ bin/server.js
@@ -1,12 +1,14 @@
+'use rstrict'
 const express = require('express');  
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const app  = express();
 const port = 8080;
+const date  = new Date();
 
 app.use(bodyParser);
 app.get("/",(req,res)=>{
-	res.send("Hello Curator :)");
+	res.send(`Hello Curator :) \n ${date.getHours}`);
 })
 
 /*
@@ -15,4 +17,5 @@ app.listen(port,()=>{
 })
 */
 console.log("curator is live ");
+module.exports = app;
 module.exports.handler = serverless(app);
